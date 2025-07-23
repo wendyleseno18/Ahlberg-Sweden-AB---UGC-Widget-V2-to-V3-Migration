1b2c952796fb30d7280d7fcbfb7e99eb8eb45fcc
WendyleChristianSeno <wendyle.seno@yotpo.com>
2025-07-23 19:30:06 +0800

    add product id

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "";
+  let ProductId = "287-500056P";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
